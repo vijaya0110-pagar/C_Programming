6263296f5af27cdf6b6b2488aefa993e281d05ea
vijaya0110-pagar <vijayapagar2006@gmail.com>
2025-11-28 02:32:14 +0530

    commit

---
+++ Assignment_32/Assignment_no32_Q4.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    { 
+        int i=0,j=0;
+        
+        for(i=1;i<=iRow;i++)
+        {
+            for(j=1;j<=iCol;j++)
+            {
+                if(j== iCol+1-i|| i==1||i==iRow ||j==1||j==iCol )
+                {
+                    System.out.print("*\t");
+                }
+                else if(j>iCol+1-i)
+                {
+                    System.out.printf("$\t");
+                }
+                else
+                {
+                    System.out.printf("#\t");
+                }  
+            }    
+            System.out.println();
+        }   
+
+    }
+}
+class Assignment_no32_Q4
+{
+    public static void main(String A[]) {
+        int iValue1=0,iValue2=0;
+        Scanner sobj=new Scanner(System.in);
+        System.out.println("enter no of rows:");
+        iValue1=sobj.nextInt();
+        System.out.println("enter no of columns:");
+        iValue2=sobj.nextInt();
+        Pattern pobj= new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
